b03a55e7fc78df10f69460312623f19c7640a299
TheDancingTriangle <thedancingtriangle@gmail.com>
2024-12-29 13:34:35 +0200

    sus153

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -984,6 +984,10 @@ struct user_struct {
 #if defined(CONFIG_PERF_EVENTS) || defined(CONFIG_BPF_SYSCALL)
 	atomic_long_t locked_vm;
 #endif
+
+#ifdef CONFIG_KSU_SUSFS
+	u64 android_kabi_reserved2;
+#endif
 };
 
 extern int uids_sysfs_init(void);
@@ -2228,6 +2232,11 @@ struct task_struct {
 	/* A live task holds one reference. */
 	atomic_t stack_refcount;
 #endif
+
+#ifdef CONFIG_KSU_SUSFS
+	u64 android_kabi_reserved8;
+#endif
+
 #ifdef OPLUS_FEATURE_UIFIRST
 	int static_ux;
 	atomic64_t dynamic_ux;
