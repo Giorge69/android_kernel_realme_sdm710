da9555d847b1fca8d06ed3d065fccbc7d4936b0a
Giorge69 <117086408+Giorge69@users.noreply.github.com>
2025-09-02 17:06:39 +0000

    susfs1510

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -387,7 +387,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		if (unlikely(inode->i_state & INODE_STATE_SUS_KSTAT)) {
+		if (unlikely(inode->i_mapping->flags & BIT_SUS_KSTAT)) {
 			susfs_sus_ino_for_show_map_vma(inode->i_ino, &dev, &ino);
 			goto bypass_orig_flow;
 		}
@@ -400,15 +400,15 @@ bypass_orig_flow:
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
         struct dentry *dentry = file->f_path.dentry;
         if (dentry) {
-        	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage")) { 
+        	const char *path = (const char *)dentry->d_name.name;
+            	if (strstr(path, "lineage")) {
 	  	start = vma->vm_start;
 		end = vma->vm_end;
 		show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
             	name = "/dev/ashmem (deleted)";
 		goto done;
             	 	}
-            	if (strstr(path, "jit-zygote-cache")) { 
+            	if (strstr(path, "jit-zygote-cache")) {
 	  	start = vma->vm_start;
 		end = vma->vm_end;
 		show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
